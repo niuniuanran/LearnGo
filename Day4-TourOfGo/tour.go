20c8c1403240d7f52cd3dd4d15c6dcd513c756d9
niuniuanran <irene.anran.niu@gmail.com>
2021-02-11 08:18:26 +1300

    Named return values

--- Day4-TourOfGo/tour.go
+++ Day4-TourOfGo/tour.go
@@ -5,8 +5,18 @@ import (
 	"math/rand"
 )
 
-func main() {
+func split(a, b int) (x, y int) {
+	x = a / b
+	y = a - (x * b)
+	return
+}
+
+func seedRandom() {
 	rand.Seed(10)
 	fmt.Println(rand.Intn(1000))
 	fmt.Println(rand.Intn(1000))
+}
+
+func main() {
+	fmt.Println(split(12, 3))
 }
