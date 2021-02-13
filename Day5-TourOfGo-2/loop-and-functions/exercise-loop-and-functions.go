c3e9520cc2797a472ca64809ead3122f7504e3f1
niuniuanran <irene.anran.niu@gmail.com>
2021-02-14 01:02:12 +1300

    Return error from sqrt

--- Day5-TourOfGo-2/loop-and-functions/exercise-loop-and-functions.go
+++ Day5-TourOfGo-2/loop-and-functions/exercise-loop-and-functions.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+// ErrNegativeSqrt is an error when trying to do Sqrt on negative number
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
+}
+
 // Sqrt calculates an estimation of the square root of the given float64 x
-func Sqrt(x float64) float64 {
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
 	sqr := 1.0
 	change := sqr
 	for change*change > 1e-10 {
 		change = (sqr*sqr - x) / (2 * sqr)
 		sqr = sqr - change
 	}
-	return sqr
+	return sqr, nil
 }
 
 func main() {
 	var x float64
 	fmt.Println("Please enter a number")
 	fmt.Scanf("%f", &x)
-	fmt.Printf("My sqrt: %16f\n", Sqrt(x))
-	fmt.Printf("math lib: %15f\n", math.Sqrt(x))
+	fmt.Println(Sqrt(x))
 }
