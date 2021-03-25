23897ed1f07baeb14d025cb7952112df25072d96
niuniuanran <irene.anran.niu@gmail.com>
2021-03-25 23:34:53 +1300

    Add NoSurf middleware

--- Day17-share-data-with-templates/cmd/web/middleware.go
+++ Day17-share-data-with-templates/cmd/web/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/justinas/nosurf"
 	"net/http"
 )
 
@@ -10,4 +11,15 @@ func WriteToConsole(next http.Handler) http.Handler {
 		fmt.Println("Hit the page")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csfHandler := nosurf.New(next)
+	csfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csfHandler
 }
--- Day17-share-data-with-templates/cmd/web/routes.go
+++ Day17-share-data-with-templates/cmd/web/routes.go
@@ -12,6 +12,7 @@ func routes(app *config.AppConfig) http.Handler{
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.Repo.ServeHome)
 	mux.Get("/about", handlers.Repo.ServeAbout)
 	return mux
