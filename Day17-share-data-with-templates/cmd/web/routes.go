f8de8948dbd429947b9809ace034929378f2657f
niuniuanran <irene.anran.niu@gmail.com>
2021-03-25 23:30:58 +1300

    Create my first middleware

--- Day17-share-data-with-templates/cmd/web/routes.go
+++ Day17-share-data-with-templates/cmd/web/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github/niuniuanran/Day17/pkg/config"
 	"github/niuniuanran/Day17/pkg/handlers"
 	"net/http"
@@ -9,6 +10,8 @@ import (
 
 func routes(app *config.AppConfig) http.Handler{
 	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
 	mux.Get("/", handlers.Repo.ServeHome)
 	mux.Get("/about", handlers.Repo.ServeAbout)
 	return mux
